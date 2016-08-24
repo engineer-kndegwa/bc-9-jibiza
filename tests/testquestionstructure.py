bd28a34422dfd4150afd9be400581feffe1d5d7b
mrkimanindegwa <mrkimanindegwa@gmail.com>
2016-08-25 00:37:58 +0300

    Adds travis and tests before merge

---
+++ tests/testquestionstructure.py
@@ -0,0 +1,15 @@
+import unittest
+from Questions.questions import QuestionStructure
+
+
+class TestQuestionStructure(unittest.TestCase):
+    def test_is_instance(self):
+        obj = QuestionStructure('Q1', 'A1', 'C1')
+        self.assertIsInstance(obj, QuestionStructure)
+
+    def test_empty_string_answer(self):
+        obj2 = QuestionStructure('Q2', 'A2', '')
+        self.assertTrue('You haven\'t answered the question', obj2)
+
+if __name__ == "__main__":
+    unittest.main()
